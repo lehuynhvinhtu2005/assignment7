234ecf4b3b150ef82784b691fb930203f708ab83
vit2604 <Dangtrungvuong2020@gmail.com>
2024-08-30 21:54:10 +0700

    assginment7

--- Assignment7.java
+++ Assignment7.java
@@ -1,59 +1,54 @@
-
 package assignment7;
 
 import java.util.Scanner;
 import java.util.Arrays;
-import java.util.ArrayList;
 
 public class Assignment7 {
-    public static float[] importData(float[] mangsothuc) {
+
+    public static void importdata(float[] array) {
         Scanner keyboard = new Scanner(System.in);
-        
-        for(int i=0;i<mangsothuc.length;i++)
-        {
-            System.out.print("Nhap phan tu thu "+(i+1)+":");
-            mangsothuc[i]=keyboard.nextFloat();   
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Phan tu thu " + (i + 1) + " la: ");
+            array[i] = keyboard.nextInt();
         }
-        return mangsothuc;
     }
-    public static void printData(float[] mangsothuc) {
-        System.out.print("Mang vua nhap la : "+Arrays.toString(mangsothuc));
-         System.out.println();
-    }    
-    public static float findMax2(float[] mangsothuc) {
-        float max1=Float.NEGATIVE_INFINITY;
-        float max2=Float.NEGATIVE_INFINITY;
-        for (int i=0;i<mangsothuc.length;i++) {
-            if (mangsothuc[i] > max1) {
-                max2 = max1;
-                max1 = mangsothuc[i];
-            } else if (mangsothuc[i] > max2 && mangsothuc[i] < max1) {
-                max2 = mangsothuc[i];
+
+    public static void printdata(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static float findmax2(float[] array) {
+        Arrays.sort(array);
+        return array[array.length - 2];
+    }
+
+    public static void deleteOddNumber(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] = 0;
             }
         }
-        return max2;
-    }
-    public static void deleteOddNumber(float[] mangsothuc) {
-        float[] mangchan = new float[mangsothuc.length];
-        for (int i=0;i<mangsothuc.length;i++) {
-            if (mangsothuc[i] % 2 == ) {
-                for(int j=0;j<mangsothuc.length;j++){
-                    mangchan[j]=mangsothuc[i];
-                }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                System.out.print(array[i] + " ");
             }
         }
-        Arrays.toString(mangchan);
+        System.out.println();
     }
+
     public static void main(String[] args) {
-        System.out.print("Nhap so phan tu trong mang : ");
         Scanner keyboard = new Scanner(System.in);
-        int a=keyboard.nextInt();
-        float[] arrays = new float[a];
-        importData(arrays);
-        printData(arrays);
-        System.out.println("Phan tu lon thu 2 trong mang la : "+findMax2(arrays));
-        System.out.println("Mang sau khi xoa cac so le la : ");
-        deleteOddNumber(arrays);
+        System.out.print("So phan tu la: ");
+        int n = keyboard.nextInt();
+        float[] number = new float[n];
+        importdata(number);
+        System.out.print("Mang sau khi nhap: ");
+        printdata(number);
+        System.out.println("Phan tu lon thu 2 cua mang la: " + findmax2(number));
+        deleteOddNumber(number);
     }
-    
+
 }
